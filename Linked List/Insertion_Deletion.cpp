8d68dc080880775c1487e0b5fae03a72e940ac03
Akshit Jain <akshitjain38630@gmail.com>
2023-10-10 22:53:47 +0530

    new files

---
+++ Linked List/Insertion_Deletion.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data ;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this ->next=NULL;
+    }
+};
+
+
+
+int main(){
+
+    Node* node1 =  new Node(5);
+
+
+
+}
+
+
+
+
+
+
+
+
